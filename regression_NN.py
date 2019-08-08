5d94bf2a46f2e475b4533366f6cff096faec6c9c
miwan <45757826+minglwang@users.noreply.github.com>
2019-08-08 11:00:22 +0200

    modify

--- regression_NN.py
+++ regression_NN.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 10:56:30 2019
+
+@author: miwan
+"""
 # import libaries
 import torch
 from torch.autograd import Variable
@@ -15,14 +21,37 @@ plt.scatter(x.numpy(),y.numpy())
 plt.show()
 
 class Net(torch.nn.Module):
-    def __init__(self,n_features,n_hidden,n_output)：
+    def __init__(self,n_features,n_hidden,n_output):
         super(Net,self).__init__()
         self.hidden = torch.nn.Linear(n_features, n_hidden)
         self.predict = torch.nn.Linear(n_hidden, n_output)
 
-    def forward(self):
+    def forward(self,x):
         x = F.relu(self.hidden(x))
         x = self.predict(x)
         return x
+
 net = Net(1,10,1)
 print(net)
+
+plt.ion()
+plt.show()
+optimizer = torch.optim.SGD(net.parameters(), lr = 0.5)
+loss_func = torch.nn.MSELoss()
+
+for t in range(100):
+    prediction = net(x)
+    loss = loss_func(prediction, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if t % 5 == 0:
+        # plot and show learning process
+        plt.cla()
+        plt.scatter(x.data.numpy(), y.data.numpy())
+        plt.plot(x.data.numpy(),prediction.data.numpy(),'r-',0.25)
+#        plt.text(0.5,0, "Loss = %.4f" % loss.data[0])
+        plt.pause(0.1)
+    plt.ioff()
+    plt.show()
+
